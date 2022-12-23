4794ffe031caeacd12e65cedbc3ba1225a95044b
Hlor1d <92319391+Hlor1d@users.noreply.github.com>
2022-12-23 12:11:30 +0300

    Add files via upload

---
+++ Task3/Migrations/20221223050326_init.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task3.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "mydatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    data = table.Column<byte[]>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_mydatas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "myFaces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Hash = table.Column<int>(type: "INTEGER", nullable: false),
+                    Embedding = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    myImageId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_myFaces", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_myFaces_mydatas_myImageId",
+                        column: x => x.myImageId,
+                        principalTable: "mydatas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_myFaces_myImageId",
+                table: "myFaces",
+                column: "myImageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "myFaces");
+
+            migrationBuilder.DropTable(
+                name: "mydatas");
+        }
+    }
+}
---
+++ Task3/Window1.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task3
+{
+    public partial class Window1 : Window
+    {
+        public ObservableCollection<myFace> myFaces1 { get; set; }
+        public ObservableCollection<mydata> mydatas1 { get; set; }
+        public ObservableCollection<System.Windows.Controls.Image> myFaces1Img { get; set; }
+        public Window1()
+        {
+            myFaces1 = new ObservableCollection<myFace>();
+            mydatas1 = new ObservableCollection<mydata>();
+            myFaces1Img = new ObservableCollection<System.Windows.Controls.Image>();
+            using var Lib = new LibraryContext();
+            var a = Lib.myFaces;
+            foreach (var b in a)
+            {
+                myFaces1.Add(b);
+            }
+            var c = Lib.mydatas;
+            foreach (var b in c)
+            {
+                mydatas1.Add(b);
+                var memoryStream = new MemoryStream(b.data);
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.StreamSource = memoryStream;
+                 bitmap.EndInit();
+                System.Windows.Controls.Image image = new()
+                {
+                    Source = bitmap
+                };
+                 myFaces1Img.Add(image);
+            }
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void ButtonDel_Click(object sender, RoutedEventArgs e)
+        {
+                using var Lib = new LibraryContext();
+                Lib.myFaces.Remove(myFaces1[List.SelectedIndex]);
+                Lib.mydatas.Remove(mydatas1[List.SelectedIndex]);
+                Lib.SaveChanges();
+                myFaces1Img.RemoveAt(List.SelectedIndex);
+        }
+    }
+}
